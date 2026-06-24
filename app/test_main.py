570af6140b9f164370548af557915ae0ef3c85f4
Andrii Deneha <deneha.andrii@gmail.com>
2026-06-24 12:39:04 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,17 @@
-from app.main import get_human_age
+import app.main as main
 
 
 def test_get_human_age_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+    assert main.get_human_age(0, 0) == [0, 0]
 
 
 def test_get_human_age_first_year() -> None:
-    assert get_human_age(15, 15) == [1, 1]
+    assert main.get_human_age(15, 15) == [1, 1]
 
 
 def test_get_human_age_second_year() -> None:
-    assert get_human_age(24, 24) == [2, 2]
+    assert main.get_human_age(24, 24) == [2, 2]
 
 
 def test_get_human_age_third_year() -> None:
-    assert get_human_age(28, 29) == [3, 3]
+    assert main.get_human_age(15, 15) == [0, 0]
