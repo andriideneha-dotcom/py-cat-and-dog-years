37874fafb6037482515d9d59855e4049106bd4af
Andrii Deneha <deneha.andrii@gmail.com>
2026-06-24 11:52:49 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -21,7 +21,6 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
     # TODO: Implement this function
     # Write your tests first, then implement the logic
     # return [0, 0]
-
     cat_result = 0
     dog_result = 0
     if cat_age < 15:
@@ -32,7 +31,6 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
         cat_result = 2
     else:
         cat_result = 2 + (cat_age - 24) // 4
-
     if dog_age < 15:
         dog_result = 0
     elif 15 <= dog_age < 24:
@@ -43,5 +41,3 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
         dog_result = 2 + (dog_age - 24) // 5
 
     return [cat_result, dog_result]
-
-
--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,17 @@
 from app.main import get_human_age
 
 
-def test_get_human_age_zero():
+def test_get_human_age_zero() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
 
-def test_get_human_age_first_year():
+def test_get_human_age_first_year() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
 
-def test_get_human_age_second_year():
+def test_get_human_age_second_year() -> None:
     assert get_human_age(24, 24) == [2, 2]
 
 
-def test_get_human_age_third_year():
+def test_get_human_age_third_year() -> None:
     assert get_human_age(28, 29) == [3, 3]
